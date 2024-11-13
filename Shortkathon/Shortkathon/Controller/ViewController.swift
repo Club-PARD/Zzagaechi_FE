b95cb4052f0f7e9ea386c9b3cd7294f01e780efa
minjun kim <minzzzun@gmail.com>
2024-11-14 00:51:55 +0900

    기본 파일 생성

---
+++ Shortkathon/Shortkathon/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shortkathon
+//
+//  Created by 김민준 on 11/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Shortkathon/ShortkathonTests/ShortkathonTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShortkathonTests.swift
+//  ShortkathonTests
+//
+//  Created by 김민준 on 11/14/24.
+//
+
+import Testing
+@testable import Shortkathon
+
+struct ShortkathonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
