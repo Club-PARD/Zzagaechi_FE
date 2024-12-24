4c3991b0db69c129cf903b688fb2fbc3e08cd1ac
minjun kim <minzzzun@gmail.com>
2024-12-25 02:37:51 +0900

    민준 : DetailPlan 폴더 및 DetailPlanViewController 생성

--- Shortkathon/Shortkathon/Controller/DetailPlan/DetailPlanViewController.swift
+++ Shortkathon/Shortkathon/Controller/DetailPlan/DetailPlanViewController.swift
@@ -4,6 +4,9 @@ class DetailPlanViewController : UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.title = "Detail Plan"
+        
+        view.backgroundColor = .white
         setUI()
     }
     
