9f100915e4aee1af04d6f4f22db40bc3ce6ed555
minjun kim <minzzzun@gmail.com>
2024-12-25 17:28:35 +0900

    [Feat] : 요일나누기 섹션View에서 messageLabel 추가

--- Shortkathon/Shortkathon/Controller/DetailPlan/SeperateDayListView.swift
+++ Shortkathon/Shortkathon/Controller/DetailPlan/SeperateDayListView.swift
@@ -3,17 +3,26 @@ import UIKit
 
 class SeperateDayListView : UIView {
     //MARK: - property
+    var data: [String] = []
     
-    let messageLabel : UILabel() = {
+    let messageLabel : UILabel = {
         let label = UILabel()
-        
+        label.text = "요일을 선택하고 세분화 목록을 여기로 드래그 하세요!"
+        label.textColor = #colorLiteral(red: 0.7294117647, green: 0.7294117647, blue: 0.7294117647, alpha: 1)
+        label.font = UIFont(name: "Pretendard-Regular", size:  12)
         return label
     }()
     
     
+    let seperateListTableView : UITableView = {
+        let view = UITableView()
+        return view
+    }()
+    
     //MARK: - main
     override init(frame: CGRect) {
         super.init(frame: frame)
+        updateUI()
         setUI()
     }
     
@@ -22,7 +31,26 @@ class SeperateDayListView : UIView {
     }
     
     //MARK: - function
+    func updateUI(){
+        if data.isEmpty{
+            messageLabel.isHidden = false
+            seperateListTableView.isHidden = true
+        } else {
+            messageLabel.isHidden = true
+            seperateListTableView.isHidden = false
+        }
+
+    }
+    
     func setUI(){
+        [messageLabel,seperateListTableView].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            self.addSubview($0)
+        }
         
+        NSLayoutConstraint.activate([
+            messageLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+        ])
     }
 }
