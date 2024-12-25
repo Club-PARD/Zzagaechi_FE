6bf512327564dac22ba9e70067162cb0f49f52fb
minjun kim <minzzzun@gmail.com>
2024-12-25 14:00:05 +0900

    [Chore] : 시작화면 위치 변경

--- Shortkathon/Shortkathon/Controller/DetailPlan/DetailPlanViewController.swift
+++ Shortkathon/Shortkathon/Controller/DetailPlan/DetailPlanViewController.swift
@@ -2,15 +2,51 @@ import UIKit
 
 class DetailPlanViewController : UIViewController {
     
+    
+    //MARK: - main
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.title = "Detail Plan"
+        view.backgroundColor = #colorLiteral(red: 0.2128768861, green: 0.2128768861, blue: 0.2128768861, alpha: 1)
         
-        view.backgroundColor = .white
+        configureNavigationBar()
+        
         setUI()
     }
     
+    func configureNavigationBar(){
+        navigationItem.title = "새로운 일 분류"
+        
+        let backButton = UIBarButtonItem(
+            title: "생성",
+            style: .plain,
+            target: self,
+            action: #selector(backButtonTapped)
+        )
+        navigationItem.leftBarButtonItem = backButton
+        
+        // 오른쪽 버튼 설정 (완료)
+        let doneButton = UIBarButtonItem(
+            title: "완료",
+            style: .done,
+            target: self,
+            action: #selector(doneButtonTapped)
+        )
+        navigationItem.rightBarButtonItem = doneButton
+    }
+
     func setUI(){
+        
+    }
+    
+    
+    @objc func backButtonTapped(){
+        print("뒤로가기")
+        navigationController?.popViewController(animated: true)
+    }
+    
+    
+    @objc func doneButtonTapped(){
+       print("완료")
         
     }
 }
