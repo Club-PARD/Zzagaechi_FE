0af8b3a92a0bf945a1563184acce1a9f69f0ef70
minjun kim <minzzzun@gmail.com>
2024-12-28 13:20:20 +0900

    [Feat] : tabbar icon 및 쉐도우 설정

--- Shortkathon/Shortkathon/Controller/MainViewController.swift
+++ Shortkathon/Shortkathon/Controller/MainViewController.swift
@@ -3,7 +3,7 @@ import UIKit
 class MainViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .gray
+        view.backgroundColor = #colorLiteral(red: 0.137254902, green: 0.137254902, blue: 0.137254902, alpha: 1)
     }
 }
 
