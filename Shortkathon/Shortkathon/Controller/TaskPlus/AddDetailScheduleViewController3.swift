7daef00eb3be3f1867b9b27faff53aa70f22cf49
minjun kim <minzzzun@gmail.com>
2024-12-30 00:39:48 +0900

    [Feat] : 세분화page3 버튼 로직 추가

--- Shortkathon/Shortkathon/Controller/TaskPlus/AddDetailScheduleViewController3.swift
+++ Shortkathon/Shortkathon/Controller/TaskPlus/AddDetailScheduleViewController3.swift
@@ -62,6 +62,7 @@ class AddDetailScheduleViewController3 : UIViewController {
         button.backgroundColor = #colorLiteral(red: 0.5591031909, green: 0.571234405, blue: 0.5998923779, alpha: 1)
         button.layer.cornerRadius = 12
         button.tintColor = .white
+        button.isEnabled = false
         return button
     }()
     
@@ -80,15 +81,21 @@ class AddDetailScheduleViewController3 : UIViewController {
         return view
     }()
     
+    private var hasTableContent: Bool = false {
+        didSet {
+            updateNextButtonState()
+        }
+    }
     
     //MARK: - main
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.1372549087, green: 0.1372549087, blue: 0.1372549087, alpha: 1)
-//        setupScrollView()
+        //        setupScrollView()
         setUI()
         buttonTapped()
         setupKeyboardDismiss()
+        setupTableViewDelegate()
     }
     
     
@@ -106,7 +113,7 @@ class AddDetailScheduleViewController3 : UIViewController {
         
         tableUIView.viewHeightConstraint = tableUIView.heightAnchor.constraint(equalToConstant: 280)
         tableUIView.viewHeightConstraint?.isActive = true
-
+        
         NSLayoutConstraint.activate([
             // ScrollView
             scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -120,33 +127,33 @@ class AddDetailScheduleViewController3 : UIViewController {
             contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
             contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
             contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
-
+            
             // MainLabel
             mainLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
             mainLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
-
+            
             // BackButton
             backButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
             backButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 33),
-
+            
             // CancelButton
             cancelButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
             cancelButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -28),
-
+            
             // ProgressBarImage
             progessbarImage.topAnchor.constraint(equalTo: mainLabel.bottomAnchor, constant: 60),
             progessbarImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 31),
-
+            
             // TitleLabel
             titleLabel.topAnchor.constraint(equalTo: progessbarImage.bottomAnchor, constant: 15),
             titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 31),
-
+            
             // TableUIView
             tableUIView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 98),
             tableUIView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 31),
             tableUIView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -31),
             tableUIView.viewHeightConstraint!,
-
+            
             // NextButton
             nextButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
             nextButton.topAnchor.constraint(equalTo: tableUIView.bottomAnchor, constant: 20),
@@ -154,6 +161,48 @@ class AddDetailScheduleViewController3 : UIViewController {
             nextButton.widthAnchor.constraint(equalToConstant: 331),
             nextButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20) // 중요
         ])
+    }
+    
+    private func setupTableViewDelegate() {
+           tableUIView.delegate = self
+       }
+    
+    private func updateNextButtonState() {
+        if hasTableContent {
+            nextButton.isEnabled = true
+            applyGradient(to: nextButton, colors: [
+                #colorLiteral(red: 0.5294117647, green: 0.6745098039, blue: 0.9411764706, alpha: 1).cgColor,
+                #colorLiteral(red: 0.3098039216, green: 0.5019607843, blue: 0.8431372549, alpha: 1).cgColor
+            ])
+        } else {
+            nextButton.isEnabled = false
+            applyGradient(to: nextButton, colors: [
+                #colorLiteral(red: 0.4862745098, green: 0.4980392157, blue: 0.5294117647, alpha: 1).cgColor,
+                #colorLiteral(red: 0.4862745098, green: 0.4980392157, blue: 0.5294117647, alpha: 1).cgColor
+            ])
+        }
+    }
+    
+    private func applyGradient(to button: UIButton, colors: [CGColor]) {
+        button.layer.sublayers?.forEach { layer in
+            if layer is CAGradientLayer {
+                layer.removeFromSuperlayer()
+            }
+        }
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = colors
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+        gradientLayer.cornerRadius = button.layer.cornerRadius
+        gradientLayer.frame = button.bounds
+        
+        button.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        updateNextButtonState()
     }
     
     func buttonTapped(){
@@ -205,4 +254,35 @@ class AddDetailScheduleViewController3 : UIViewController {
         view.endEditing(true)
     }
     
+}
+
+//MARK: - seperateTaskView 델리게이트
+
+extension AddDetailScheduleViewController3: SeperateTaskViewDelegate {
+    func taskContentDidChange(hasContent: Bool) {
+            print("taskContentDidChange called: \(hasContent)")
+            if hasContent {
+                nextButton.isEnabled = true
+                applyGradient(to: nextButton, colors: [
+                    #colorLiteral(red: 0.5294117647, green: 0.6745098039, blue: 0.9411764706, alpha: 1).cgColor,
+                    #colorLiteral(red: 0.3098039216, green: 0.5019607843, blue: 0.8431372549, alpha: 1).cgColor
+                ])
+            } else {
+                nextButton.isEnabled = false
+                applyGradient(to: nextButton, colors: [
+                    #colorLiteral(red: 0.4862745098, green: 0.4980392157, blue: 0.5294117647, alpha: 1).cgColor,
+                    #colorLiteral(red: 0.4862745098, green: 0.4980392157, blue: 0.5294117647, alpha: 1).cgColor
+                ])
+            }
+            
+            // 레이아웃 업데이트
+            nextButton.setNeedsDisplay()
+            nextButton.layoutIfNeeded()
+        }
+    
+}
+
+// SeperateTaskView에 추가할 델리게이트 프로토콜
+protocol SeperateTaskViewDelegate: AnyObject {
+    func taskContentDidChange(hasContent: Bool)
 }
--- Shortkathon/Shortkathon/Controller/View/SeperateTaskView.swift
+++ Shortkathon/Shortkathon/Controller/View/SeperateTaskView.swift
@@ -4,6 +4,7 @@ import UIKit
 class SeperateTaskView : UIView {
     //MARK: - property
     var task : [String] = [""]
+    weak var delegate: SeperateTaskViewDelegate?
     
     var viewHeightConstraint : NSLayoutConstraint?
     
@@ -14,7 +15,7 @@ class SeperateTaskView : UIView {
     private let bottomPadding: CGFloat = 6.5  // 하단 여백
     
     
-
+    
     
     let seperateTaskTableView : UITableView = {
         let view = UITableView()
@@ -32,7 +33,7 @@ class SeperateTaskView : UIView {
         return button
     }()
     
- 
+    
     
     
     //MARK: - main
@@ -42,7 +43,7 @@ class SeperateTaskView : UIView {
         setUI()
         touchAction()
         plusTaskButton.addTarget(self, action:  #selector(plusAction), for: .touchUpInside)
-
+        
     }
     
     required init?(coder: NSCoder) {
@@ -57,7 +58,7 @@ class SeperateTaskView : UIView {
         }
         
         NSLayoutConstraint.activate([
-          
+            
             seperateTaskTableView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor , constant: 21),
             seperateTaskTableView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 9),
             seperateTaskTableView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -9),
@@ -82,6 +83,18 @@ class SeperateTaskView : UIView {
         self.addGestureRecognizer(tapGesture)
     }
     
+    private func notifyContentChange() {
+        // 테이블뷰에 내용이 있는지 확인
+        let hasContent = !task.contains{ $0.isEmpty}
+        print("Tasks: \(task)")
+         print("Has content: \(hasContent)")
+        delegate?.taskContentDidChange(hasContent: hasContent)
+    }
+    
+    // 셀이 추가되거나 삭제될 때마다 호출
+    func updateContent() {
+        notifyContentChange()
+    }
     //view의 높이 계산
     private func calculateViewHeight() -> CGFloat {
         let tableViewHeight = calculateTableViewHeight()
@@ -99,42 +112,159 @@ class SeperateTaskView : UIView {
     
     
     func updateHeight() {
-            let tableHeight = seperateTaskTableView.contentSize.height
-            
-            let totalHeight = tableHeight // 버튼 높이(44) + 여백(10) 포함
-            viewHeightConstraint?.constant = totalHeight
-            // 상위 스크롤뷰에 레이아웃 업데이트 알림
-            superview?.layoutIfNeeded()
-        }
+        let tableHeight = seperateTaskTableView.contentSize.height
+        
+        let totalHeight = tableHeight // 버튼 높이(44) + 여백(10) 포함
+        viewHeightConstraint?.constant = totalHeight
+        // 상위 스크롤뷰에 레이아웃 업데이트 알림
+        superview?.layoutIfNeeded()
+    }
     
     
     
     // 높이 업데이트 함수
     private func updateViewHeight() {
-            viewHeightConstraint?.constant = calculateViewHeight()
-            
-            UIView.animate(withDuration: 0.3) {
-                self.superview?.layoutIfNeeded()
-            }
+        viewHeightConstraint?.constant = calculateViewHeight()
+        
+        UIView.animate(withDuration: 0.3) {
+            self.superview?.layoutIfNeeded()
         }
- 
+    }
+    
     
     @objc private func handleBackgroundTap() {
         print(task)
         self.endEditing(true)
     }
     
-    @objc private func plusAction(){
+//    @objc private func plusAction(){
+//        task.append("")
+//        print(task)
+//        seperateTaskTableView.reloadData()
+//        updateViewHeight()
+//        
+//    }
+    
+    @objc private func plusAction() {
         task.append("")
         print(task)
         seperateTaskTableView.reloadData()
         updateViewHeight()
-
+        notifyContentChange()
     }
+    
+    
+    
 }
 
 
 //MARK: - TableView Extension
+//extension SeperateTaskView : UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return task.count
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ThirdTableCell", for: indexPath) as? SeperateTaskViewTableCell else {
+//            
+//            return UITableViewCell()
+//        }
+//        
+//        cell.taskTextField.text = task[indexPath.row]
+//        cell.taskTextField.tag = indexPath.row
+//        cell.taskTextField.delegate = self
+//        
+//        // 셀과 텍스트필드의 사용자 상호작용 활성화
+//        cell.isUserInteractionEnabled = true
+//        cell.taskTextField.isUserInteractionEnabled = true
+//        
+//        return cell
+//    }
+//    
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
+//    
+//    
+//    
+//    func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+//        updateHeight()
+//    }
+//}
+//
+//
+////MARK: - textField extension
+//extension SeperateTaskView : UITextFieldDelegate {
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//        // 텍스트 변경될 때마다 호출
+//        let currentText = textField.text ?? ""
+//        let updatedText = (currentText as NSString).replacingCharacters(in: range, with: string)
+//        task[textField.tag] = updatedText
+//        notifyContentChange()
+//        return true
+//    }
+//
+//    func textFieldDidEndEditing(_ textField: UITextField) {
+//        task[textField.tag] = textField.text ?? ""
+//        notifyContentChange()
+//    }
+//
+//   
+//
+//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == .delete {
+//            task.remove(at: indexPath.row)
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//            
+//            if task.isEmpty {
+//                task.append("")
+//                tableView.reloadData()
+//            }
+//            updateViewHeight()
+//            notifyContentChange()
+//        }
+//    }
+//    
+//    
+//    //textFiled 편집되었을떄 호출
+////
+//    func textFieldDidBeginEditing(_ textField: UITextField) {
+//        // 텍스트필드 편집 시작할 때 호출
+//        print("텍스트필드 편집 시작")
+//    }
+//    
+//
+////    
+//    // Enter 눌렀을때 return
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        textField.resignFirstResponder()
+//        return true
+//    }
+//    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 57
+//    }
+//    
+//    
+//    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+//        return true
+//    }
+//    
+//    // 스와이프 스타일 설정
+//    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+//        return .delete
+//    }
+//
+//    
+//    func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+//        return "삭제"
+//    }
+//}
+//
+
+
+
+
 extension SeperateTaskView : UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return task.count
@@ -142,7 +272,6 @@ extension SeperateTaskView : UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ThirdTableCell", for: indexPath) as? SeperateTaskViewTableCell else {
-            
             return UITableViewCell()
         }
         
@@ -150,73 +279,35 @@ extension SeperateTaskView : UITableViewDelegate, UITableViewDataSource {
         cell.taskTextField.tag = indexPath.row
         cell.taskTextField.delegate = self
         
-        // 셀과 텍스트필드의 사용자 상호작용 활성화
         cell.isUserInteractionEnabled = true
         cell.taskTextField.isUserInteractionEnabled = true
         
         return cell
     }
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    
-    
-    func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-          updateHeight()
-      }
-}
-
-
-//MARK: - textField extension
-extension SeperateTaskView : UITextFieldDelegate {
-    //textFiled 편집되었을떄 호출
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        // 텍스트필드 편집 시작할 때 호출
-        print("텍스트필드 편집 시작")
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        task[textField.tag] = textField.text ?? ""
-    }
-    
-    // Enter 눌렀을때 return
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 57
     }
-    
     
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
     
-    // 스와이프 스타일 설정
     func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
         return .delete
     }
     
-    // 삭제 액션 처리
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // 데이터 소스에서 항목 삭제
             task.remove(at: indexPath.row)
-            
-            // TableView에서 행 삭제
             tableView.deleteRows(at: [indexPath], with: .fade)
             
-            // task 배열이 비어있으면 빈 문자열 추가
             if task.isEmpty {
                 task.append("")
                 tableView.reloadData()
             }
             updateViewHeight()
+            notifyContentChange()
         }
     }
     
@@ -225,5 +316,23 @@ extension SeperateTaskView : UITextFieldDelegate {
     }
 }
 
-
-
+// MARK: - TextField Extension
+extension SeperateTaskView : UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentText = textField.text ?? ""
+        let updatedText = (currentText as NSString).replacingCharacters(in: range, with: string)
+        task[textField.tag] = updatedText
+        notifyContentChange()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        task[textField.tag] = textField.text ?? ""
+        notifyContentChange()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
