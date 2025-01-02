79016d61023f12b1adcdf0602bf73d8a6dba1b57
minjun kim <minzzzun@gmail.com>
2025-01-03 03:48:04 +0900

    [Fix] : 위젯 만들기 전 세팅

--- Shortkathon/Shortkathon/SceneDelegate.swift
+++ Shortkathon/Shortkathon/SceneDelegate.swift
@@ -29,7 +29,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         } else {
             // 로그인하지 않은 상태라면 AppleLoginViewController로 이동
             print("아직 로그인")
-            let loginVC = ViewController()
+            let loginVC = AppleLoginViewController()
             loginVC.modalPresentationStyle = .fullScreen
             window.rootViewController = loginVC
         }
